36c7fdc97e03b1a9736da81633b3fac7044fe31c
shivam-0912 <shivamtayal1992@gmail.com>
2020-09-16 10:21:03 +0530

    Updates

---
+++ precache-manifest.fa95e52c07dcc9806c94ba11ef93e3ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a423dd9e607275199bd0eb2e156c842",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5df0b510ef3b48922fe2",
+    "url": "/static/css/main.4c12b03d.chunk.css"
+  },
+  {
+    "revision": "537c3136d4de4a3272a0",
+    "url": "/static/js/2.3c6210d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3c6210d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5df0b510ef3b48922fe2",
+    "url": "/static/js/main.4a7cddd4.chunk.js"
+  },
+  {
+    "revision": "ea3804e7cc1ad433d06c",
+    "url": "/static/js/runtime-main.4ad5ec28.js"
+  },
+  {
+    "revision": "0e878a0fa68c61b06e781cee2e6bc71f",
+    "url": "/static/media/WhatsApp.0e878a0f.svg"
+  }
+]);
